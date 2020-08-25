781c120c1096862b04e8ee6db61cc190ab382b85
Tianyang Zhou <tzu@mm-software.com>
2020-08-25 16:34:59 +0800

    Initial commit

---
+++ Identity.Core/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Identity.Core.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Identity.Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly UserManager<IdentityCoreUser> _manager;
+
+        public AccountController(ILogger<AccountController> logger, UserManager<IdentityCoreUser> manager)
+        {
+            _logger = logger;
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public IEnumerable<IdentityCoreUser> Get()
+        {
+            return _manager.Users.ToArray();
+        }
+    }
+}
---
+++ Identity.Core/Startup.cs
@@ -0,0 +1,60 @@
+using Identity.Core.Areas.Identity.Data;
+using Identity.Core.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Identity.Core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<IdentityCoreContext>(options =>
+                    options.UseSqlite(Configuration.GetConnectionString("IdentityCoreContextConnection")));
+
+            services.AddIdentity<IdentityCoreUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<IdentityCoreContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddControllers();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/account/login";
+                options.LogoutPath = $"/account/logout";
+                options.AccessDeniedPath = $"/account/access-denied";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
