d298d1ed50c924bfb1990168074a96b2a99e52dd
Yarn <ztytotoro@users.noreply.github.com>
2020-08-27 00:15:01 +0800

    add i18n

--- ErrorHandler/ErrorHandlerExtension.cs
+++ ErrorHandler/ErrorHandlerExtension.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 
 namespace ErrorHandler
 {
@@ -8,12 +11,33 @@ namespace ErrorHandler
     {
         public static void UseExceptionFilter(this MvcOptions options)
         {
-            options.Filters.Add(new HttpResponseExceptionFilter());
+            options.Filters.Add<HttpResponseExceptionFilter>();
+        }
+
+        public static void AddLocalization(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+        }
+
+        public static void UseLocalization(this IApplicationBuilder app)
+        {
+            var supportedCultures = new[] { "en-US", "zh-CN" };
+            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            app.UseRequestLocalization(localizationOptions);
         }
     }
 
     public class HttpResponseExceptionFilter : IActionFilter, IOrderedFilter
     {
+        private readonly IStringLocalizer<HttpResponseExceptionFilter> _localizer;
+
+        public HttpResponseExceptionFilter(IStringLocalizer<HttpResponseExceptionFilter> localizer) {
+            _localizer = localizer;
+        }
+
         public int Order { get; set; } = int.MaxValue - 10;
 
         public void OnActionExecuting(ActionExecutingContext context) { }
@@ -24,7 +48,8 @@ namespace ErrorHandler
             {
                 context.Result = new ObjectResult(new
                 {
-                    errorCode = exception.Code
+                    errorCode = exception.Code,
+                    message = _localizer[((int)exception.Code).ToString()]
                 })
                 {
                     StatusCode = StatusCodes.Status400BadRequest,
