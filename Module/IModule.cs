6c3635e0a78f25f87d12badc29fda15185e268df
Tianyang Zhou <tzu@mm-software.com>
2020-08-28 18:01:20 +0800

    try reflection

---
+++ Module/IModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Module
+{
+    public interface IModule
+    {
+        IConfiguration Configuration { get => null; set { } }
+
+        void ConfigureServices(IServiceCollection services) { }
+        void Configure(IApplicationBuilder app, IWebHostEnvironment env) { }
+    }
+}
---
+++ Module/TestModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Module
+{
+    public class TestModule : IModule
+    {
+        public void ConfigureServices(IServiceCollection services) {
+            Console.WriteLine("module hit");
+        }
+    }
+}
